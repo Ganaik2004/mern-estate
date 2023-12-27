67069fe3b833bc6a4206b879e4c35f8f2273d349
Ganaik2004 <137753416+Ganaik2004@users.noreply.github.com>
2023-12-27 20:04:56 +0530

    updated the profile page and protectrd the profile page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import {FaSearch} from 'react-icons/fa'
 import {Link} from 'react-router-dom'
+import {useSelector} from 'react-redux'
 export default function Header() {
+  const {currentUser} = useSelector(state=>state.user);
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -15,7 +17,7 @@ export default function Header() {
           <input type="text" placeholder="Search..." className="bg-transparent flex-1 focus:outline-none w-34 sm:w-64"/>
           <FaSearch className="text-slate-600 "/>
         </form>
-     <ul className="flex gap-4">
+     <ul className="flex gap-4 items-center">
       
 <Link to="/">
 <li className="hidden sm:inline text-slate-700 hover:underline">Home</li>
@@ -23,8 +25,10 @@ export default function Header() {
 <Link to='/about'>
 <li className="hidden sm:inline text-slate-700 hover:underline">About</li>
 </Link>
-<Link to='/sign-in'>
-<li className=" text-slate-700 hover:underline">Sign In</li>
+<Link to='/profile'>
+{
+  currentUser? (<img className="rounded-full h-8 w-8 object-cover" src={currentUser.avatar} alt="Profile Photo"></img>):(<li className=" text-slate-700 hover:underline">Sign In</li>)
+}
 </Link>
     
  
